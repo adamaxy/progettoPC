59136a312c1d0285410e17d4e2a85f467008e20d
adamaxy <adam996@libero.it>
2023-08-02 18:45:19 +0200

    ok

--- Main.java
+++ Main.java
@@ -1,3 +1,13 @@
 public class Main {
+    public static void main(String [] args){
+        objCpu cpu = new objCpu();
+        printObjCpu(cpu);
 
+    }
+
+
+    static void printObjCpu ( objCpu objCpu){
+        System.out.println("manufacturer = " + objCpu.manufacturer);
+
+    }
 }
---
+++ objCpu.java
@@ -0,0 +1,20 @@
+public class objCpu {
+    //oggetto cpu
+
+    String manufacturer = "AMD";
+    String partCode = " 100-100000910WOF";
+    int CoreCount = 8;
+
+    double coreClockGhz = 4.2;
+
+    double boostClockGhz = 5;
+
+    int woltageTdp = 120;
+    String Series = "AMD Ryzen 7";
+
+    String Socket = "AM5";
+
+   String integratedGraphics = "Readon";
+
+     String MaxMemorySupport = "128 GB";
+}
